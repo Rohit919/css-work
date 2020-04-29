a424337779a348619ea4f03efcd09c91cd9c6de9
Rohit <rohitkumar.0667@gmail.com>
2020-04-29 13:51:39 +0530

    nav bar conplete

---
+++ Nav-Bar/Nav-Bar-Responsive/script.js
@@ -0,0 +1,7 @@
+var menuToggle = document.querySelector('.menuToggle');
+var menuBar = document.querySelector('.menu-bar');
+
+menuToggle.onclick =function() {
+    menuBar.classList.toggle('active');
+}
+
